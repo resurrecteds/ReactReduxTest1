aab830566d1301b3c9dbdb9cbc81c0cd2b03b9a6
Liron <liron.moryosef@pixoneye.com>
2018-09-12 13:13:52 +0300

    example of project where mapStateToProps and mapDispatchToProps are available

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+    return [
+        { title: 'JavaScript: the good parts' },
+        { title: 'Harry Potter 1' },
+        { title: 'Harry Potter 2' },
+        { title: 'Harry Potter 3' },
+        { title: 'Tai Pan' },
+        { title: 'Gai Jin' },
+        { title: 'The dark tower' }
+    ];
+}
